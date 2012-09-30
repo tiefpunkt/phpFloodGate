5c5c19603e054b330b9b15738fb0dcd0bb73dedd
Severin Schols <severin@schols.de>
2012-09-30 11:52:28 +0200

    Persistence abstractio, perstisting via filesystem

--- floodgate.php
+++ floodgate.php
@@ -1,15 +1,17 @@
 <?php
 $limit = 1000; // in ms
+$pers_enginge = "filesystem"; // Persistence engine: filesystem or apc
 
-$key = "floodgate_time";
+$apc_key = "floodgate_time";
+$fs_filename = "/tmp/phpfloodgate.txt";
 
 function passFloodGate($dontDie = false) {
 	global $limit, $key;
 	
 	$current_request = microtime(true)*1000;
 
-	if (apc_exists($key)) {
-		$last_request = apc_fetch($key);	
+	if (_exists()) {
+		$last_request = _get();	
 		if (($current_request - $last_request) < $limit) {
 			if (!$dontDie) {
 				echo "Rate exceeded.";
@@ -19,8 +21,41 @@ function passFloodGate($dontDie = false) {
 			}
 		}
 	}
-	apc_store($key, $current_request);
+	_put($current_request);
 	return true;
 }
 
+/********************************
+ * Persistence abstraction
+ ********************************/
+ 
+function _exists() {
+	global $pers_engine, $apc_key, $fs_filename;
+	
+	if ($pers_engine == "apc") {
+		return apc_exists($apc_key);
+	} else {
+		return file_exists($fs_filename);
+	}
+}
+
+function _get() {
+	global $pers_engine, $apc_key, $fs_filename;
+	
+	if ($pers_engine == "apc") {
+		return apc_fetch($apckey);
+	} else {
+		return file_get_contents($fs_filename);
+	}
+}
+
+function _put($value) {
+	global $pers_engine, $apc_key, $fs_filename;
+	
+	if ($pers_engine == "apc") {
+		return apc_store($apc_key, $value);
+	} else {
+		return file_put_contents($fs_filename, $value);
+	}
+}
 ?>
